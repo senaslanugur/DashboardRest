3ead73330aceac630d9e21ed908617688acbc8fb
Uğur Şenaslan <senaslanugur@gmail.com>
2022-07-31 16:44:18 +0300

    image attributed added

--- js/main_scripts.js
+++ js/main_scripts.js
@@ -48,9 +48,9 @@ ref.on("value", function(snapshot) {
     var interface_2 = document.getElementById("datas_menu")
     for(var i=0; i<keys.length; i++){
         for(var k=0; k<datas[i].length; k++){
-            interface_2.innerHTML += '<tr> <td>'+keys[i]+'</td><td>'+datas[i][k].name+'</td><td>'+datas[i][k].details+'</td><td>'+datas[i][k].price+'</td><td>'+
-            '<i class="fas fa-edit" style="color:green;" id="'+keys[i] +'**'+ datas[i][k].name+'**'+datas[i][k].details+'**'+datas[i][k].price+'**'+k+'" onClick=update(this)></i>&nbsp;&nbsp;' +
-            '<i class="fas fa-trash-alt" style="color:red;"  id="'+keys[i] +'**'+ datas[i][k].name+'**'+datas[i][k].details+'**'+datas[i][k].price+'**'+k+'" onClick=remove(this)></i> </td></tr>'
+            interface_2.innerHTML += '<tr> <td  id="'+datas[i][k].image+'" onclick=image_view(this)>'+keys[i]+'</td><td>'+datas[i][k].name+'</td><td>'+datas[i][k].details+'</td><td>'+datas[i][k].price+'</td><td>'+
+            '<i class="fas fa-edit" style="color:green;" id="'+keys[i] +'**'+ datas[i][k].name+'**'+datas[i][k].details+'**'+datas[i][k].price+'**'+k+'**'+datas[i][k].image+'" onClick=update(this)></i>&nbsp;&nbsp;' +
+            '<i class="fas fa-trash-alt" style="color:red;"  id="'+keys[i] +'**'+ datas[i][k].name+'**'+datas[i][k].details+'**'+datas[i][k].price+'**'+k+'**'+datas[i][k].image+'" onClick=remove(this)></i> </td></tr>'
         }
     }
 }, function (error) {
@@ -59,14 +59,13 @@ ref.on("value", function(snapshot) {
 
 function update(d){
     var test = d.id
-    // alert(test.split("**")[2])
-    // alert(d.id)
     Swal.fire({
         title: test.split("**")[0]+ ', '+test.split("**")[1],
         html:'<input type="text" class="form-control" id="name" aria-describedby="Name" placeholder="Name" value="'+test.split("**")[1]+'"> <br>'+
             '<input type="text" class="form-control" id="details" aria-describedby="Details" placeholder="Details" value="'+test.split("**")[2]+'"> <br>'+
-            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price" value="'+test.split("**")[3]+'">',
-        imageUrl: 'https://unsplash.it/400/200',
+            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price" value="'+test.split("**")[3]+'"><br>'+
+            '<label class="btn btn-warning">  Select Image File <input type="file" id="files" name="files[]" hidden> </label>',
+        imageUrl: test.split("**")[5],
         imageWidth: 400,
         imageHeight: 200,
         imageAlt: 'Custom image',
@@ -76,23 +75,46 @@ function update(d){
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          var name = document.getElementById("name").value
-          var details = document.getElementById("details").value
-          var price = document.getElementById("price").value
-          Swal.fire(test.split("**")[0], '', 'info')
-          var path = test.split("**")[0] + "/"
-          var to_save = firebase.database().ref(path);
-            var data = [    
-                {
-                "name": name,
-                "details": details,
-                "price": parseFloat(price)
-                }
-            ]
-            to_save.update(data, function () {
-                Swal.fire("Saved", '', 'info')
-                location.reload();
+            local_storage = (JSON.stringify(local_storage))
+            local_storage = (JSON.parse(local_storage))
+            var categories = test.split("**")[0];
+            var name = document.getElementById("name").value
+            var details = document.getElementById("details").value
+            var price = document.getElementById("price").value
+            var file = document.getElementById("files").files[0]
+            var path = categories + "/" + name
+            var to_save_image = firebase.storage().ref(path)
+            let thisRef = to_save_image.child(file.name)
+            var index = parseInt(test.split("**")[4])
+            local_storage[categories].splice(index,1)
+            var ref = firebase.database().ref()
+            ref.set(local_storage, function () {
+                console.log("removed")
             })
+            thisRef.put(file).then(res=>{
+				console.log("Upload Success")
+				alert("Upload Success")
+                to_save_image.child(file.name).getDownloadURL().then(url=>{
+                    console.log(url)
+                        var to_save = firebase.database().ref();
+                        var data =     
+                            {
+                            "name": name,
+                            "details": details,
+                            "price": parseFloat(price),
+                            "image": url
+                            }
+                        
+                        local_storage[categories].push(data)
+                        console.log(local_storage)
+                        to_save.set(local_storage, function () {
+                            Swal.fire("Added", '', 'info')
+                            location.reload();
+                        })
+                })
+			}).catch(e =>{
+				console.log("Error" + e)				
+			})
         } else if (result.isDenied) {
           Swal.fire('Changes are not saved', '', 'info') 
         }
@@ -143,7 +165,8 @@ function newmenu(){
         html: drop +'<br><br>'+
             '<input type="text" class="form-control" id="name" aria-describedby="Name" placeholder="Name"> <br>'+
             '<input type="text" class="form-control" id="details" aria-describedby="Details" placeholder="Details"> <br>'+
-            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price" >',
+            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price" ><br>'+
+            '<label class="btn btn-warning">  Select Image File <input type="file" id="files" name="files[]" hidden> </label>',
         imageUrl: 'https://unsplash.it/400/200',
         imageWidth: 400,
         imageHeight: 200,
@@ -156,28 +179,39 @@ function newmenu(){
         if (result.isConfirmed) {
             local_storage = (JSON.stringify(local_storage))
             local_storage = (JSON.parse(local_storage))
-
             var select = document.getElementById("categories");
             var categories = select.options[select.selectedIndex].value;
             var name = document.getElementById("name").value
             var details = document.getElementById("details").value
             var price = document.getElementById("price").value
-            var path = categories + "/"
-            var to_save = firebase.database().ref();
-            var data =     
-                {
-                "name": name,
-                "details": details,
-                "price": parseFloat(price)
-                }
-            
-            local_storage[categories].push(data)
-            console.log(local_storage)
-            to_save.set(local_storage, function () {
-                Swal.fire("Added", '', 'info')
-                location.reload();
-            })
-
+            var file = document.getElementById("files").files[0]
+            var path = categories + "/" + name
+            var to_save_image = firebase.storage().ref(path)
+            let thisRef = to_save_image.child(file.name)
+            thisRef.put(file).then(res=>{
+				console.log("Upload Success")
+				alert("Upload Success")
+                to_save_image.child(file.name).getDownloadURL().then(url=>{
+                    console.log(url)
+                        var to_save = firebase.database().ref();
+                        var data =     
+                            {
+                            "name": name,
+                            "details": details,
+                            "price": parseFloat(price),
+                            "image": url
+                            }
+                        
+                        local_storage[categories].push(data)
+                        console.log(local_storage)
+                        to_save.set(local_storage, function () {
+                            Swal.fire("Added", '', 'info')
+                            location.reload();
+                        })
+                })
+			}).catch(e =>{
+				console.log("Error" + e)				
+			})
         } else if (result.isDenied) {
           Swal.fire('Changes are not saved', '', 'info') 
         }
@@ -186,14 +220,13 @@ function newmenu(){
 
 function newcategory(){
    
-    Swal.fire('developing...', '', 'info') 
-
     Swal.fire({
         title: "Create New Categories",
         html:'<input type="text" class="form-control" id="cat" aria-describedby="Categories" placeholder="Categories Name"> <br>'+
             '<input type="text" class="form-control" id="name" aria-describedby="Name" placeholder="First Menu Name"> <br>'+
             '<input type="text" class="form-control" id="details" aria-describedby="Details" placeholder="Details"> <br>'+
-            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price" >',
+            '<input type="number" class="form-control" id="price" aria-describedby="Price" placeholder="Price"> <br>'+
+            '<label class="btn btn-warning">  Select Image File <input type="file" id="files" name="files[]" hidden> </label>',
         imageUrl: 'https://unsplash.it/400/200',
         imageWidth: 400,
         imageHeight: 200,
@@ -211,29 +244,75 @@ function newcategory(){
             var name = document.getElementById("name").value
             var details = document.getElementById("details").value
             var price = document.getElementById("price").value
-    
-            var to_save = firebase.database().ref();
-            var data =[
-                {
-                "name": name,
-                "details": details,
-                "price": parseFloat(price)
-                }
-            ]
-            
-            local_storage[cate] = data
-            console.log(local_storage)
-            to_save.set(local_storage, function () {
-                Swal.fire("Added new categories", '', 'info')
-                location.reload();
-            })
+            var file = document.getElementById("files").files[0]
+            var path = cate + "/" + name
+            var to_save_image = firebase.storage().ref(path)
+            let thisRef = to_save_image.child(file.name)
 
+            thisRef.put(file).then(res=>{
+				console.log("Upload Success")
+				alert("Upload Success")
+                to_save_image.child(file.name).getDownloadURL().then(url=>{
+                    console.log(url)
+                        var to_save = firebase.database().ref();
+                        var data =[
+                            {
+                            "name": name,
+                            "details": details,
+                            "price": parseFloat(price),
+                            "image":url
+                            }
+                        ]
+                        
+                    local_storage[cate] = data
+                    console.log(local_storage)
+                    to_save.set(local_storage, function () {
+                        Swal.fire("Added new categories", '', 'info')
+                        location.reload();
+                    })
+                })
+			}).catch(e =>{
+				console.log("Error" + e)				
+			})
         } else if (result.isDenied) {
-            Swal.fire('Changes are not saved', '', 'info') 
+          Swal.fire('Changes are not saved', '', 'info') 
         }
-        })
+      })
+
+        //     var to_save = firebase.database().ref();
+        //     var data =[
+        //         {
+        //         "name": name,
+        //         "details": details,
+        //         "price": parseFloat(price)
+        //         }
+        //     ]
+            
+        //     local_storage[cate] = data
+        //     console.log(local_storage)
+        //     to_save.set(local_storage, function () {
+        //         Swal.fire("Added new categories", '', 'info')
+        //         location.reload();
+        //     })
+
+        // } else if (result.isDenied) {
+        //     Swal.fire('Changes are not saved', '', 'info') 
+        // }
+        // })
 }
 
 function deletecategory(){
     Swal.fire('developing...', '', 'info') 
+}
+function image_view(d){
+    var url = d.id
+    Swal.fire({
+        title: "Image of Menu",
+        imageUrl: url,
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        showCancelButton: true,
+        })
+
 }
